9dc908c508322aa93dc627b1d8195bd7701ef8dc
j3ffh95 <j3ffh95@gmail.com>
2020-08-05 12:14:52 -0400

    Finish with the generateJoke function

--- main.js
+++ main.js
@@ -5,4 +5,18 @@ const get_joke = document.querySelector("#get_joke");
 // Add event listeners to the button to get joke
 get_joke.addEventListener("click", generateJoke);
 
-async function generateJoke() {}
+generateJoke();
+
+async function generateJoke() {
+  // call the icanhaz API
+  const jokeRes = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const joke = await jokeRes.json();
+  // console.log(joke);
+  // set the new joke
+  jokeEl.innerHTML = joke.joke;
+}
