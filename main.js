96b64f7af5d5868dde4738fe2d925d36bf94a554
j3ffh95 <j3ffh95@gmail.com>
2020-08-05 12:07:30 -0400

    Added event listeners

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 // DOM Elements
 const jokeEl = document.querySelector("#joke");
 const get_joke = document.querySelector("#get_joke");
+
+// Add event listeners to the button to get joke
+get_joke.addEventListener("click", generateJoke);
+
+async function generateJoke() {}
